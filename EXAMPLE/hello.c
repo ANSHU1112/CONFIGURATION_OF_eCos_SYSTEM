4af4d5b83244a641269fe518a48b3017b2bcc97e
ANSHU <priyanshu.sharma2015@vit.ac.in>
2018-06-21 19:01:05 +0530

    Initial commit

---
+++ EXAMPLE/hello.c
@@ -0,0 +1,11 @@
+/* this is a simple hello world program */
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hello, eCos world!\n");
+  printf("Priyanshu Sharma\n");
+  printf("15BCE1282\n");
+  printf("Embedded Syatem and Design\n");
+  return 0;
+}
